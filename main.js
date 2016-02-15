c6475d4157d6245407003c604bb3bcf27c3d7be8
Seth Lytton <slytton@gmail.com>
2016-02-15 14:03:27 -0700

    Try some of the exercises in the tutorial.

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log("sanity check");
+var firstParagraph = document.getElementById('main');
+console.log(firstParagraph);
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements);
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement)
+
+
+var allListItems = document.getElementsByTagName('li');
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i]);
+}
+
+
+var evenElements = document.getElementsByClassName('even');
+console.log(evenElements)
+
+var oddElements = document.getElementsByClassName('odd');
+console.log(oddElements)
+
+var firstEvenElement = document.getElementsByClassName('even')[0];
+console.log(firstEvenElement)
+
+var firstOddElement = document.getElementsByClassName('odd')[0];
+console.log(firstOddElement)
+
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i].innerText = i);
+}
+console.log(allListItems);
+
+
+var anotherListElement = document.createElement('li');
+anotherListElement.textContent = "Prepend";
+var list = document.getElementsByTagName('ul')[0]
+list.insertBefore(anotherListElement, list.firstChild)
